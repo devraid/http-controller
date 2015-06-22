6ecf270c4dba19a1258610dd0e68c884bc3b197e
Gonzalo Chumillas <gchumillas@email.com>
2015-06-22 20:59:14 +0200

    docs: replace Soloproyectos by WirexMedia

--- src/http/controller/data/HttpControllerDataRequest.php
+++ src/http/controller/data/HttpControllerDataRequest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of Soloproyectos common library.
+ * This file is part of WirexMedia common library.
  *
  * @author  Gonzalo Chumillas <gchumillas@email.com>
  * @license https://github.com/soloproyectos/php.common-libs/blob/master/LICENSE BSD 2-Clause License
